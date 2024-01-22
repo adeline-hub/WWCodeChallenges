5bd4dc4d64dcf8139b2911a857fc912967b4820c
adeline <59871821+adeline-hub@users.noreply.github.com>
2024-01-22 19:44:20 +0100

    Update day3-Write a function to count the number of vowels in a given string.py

--- WWCodeChallenge60/day3-Write a function to count the number of vowels in a given string.py
+++ WWCodeChallenge60/day3-Write a function to count the number of vowels in a given string.py
@@ -1,7 +1,14 @@
 #Write a function to count the number of vowels in a given string
 
-string = "Coucou petit oiseau"
-vowels = "aeiouAEIOU"
- 
-count = sum(string.count(vowel) for vowel in vowels)
-print(count)
+def count_vowels(sentence):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for character in sentence:
+        if character in vowels:
+            count +=1
+    return count
+
+
+sentence = input("Enter your sentence: ")
+vowel_count = count_vowels(sentence)
+print("In the sentence, vowels count for:", vowel_count)
