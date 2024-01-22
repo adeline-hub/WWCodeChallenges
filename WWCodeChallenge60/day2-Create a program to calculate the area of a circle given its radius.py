5187f243015ea849cdccf558654082c1b6557ba1
adeline <59871821+adeline-hub@users.noreply.github.com>
2024-01-22 17:06:12 +0100

    Update day2-Create a program to calculate the area of a circle given its radius.py

--- WWCodeChallenge60/day2-Create a program to calculate the area of a circle given its radius.py
+++ WWCodeChallenge60/day2-Create a program to calculate the area of a circle given its radius.py
@@ -1,1 +1,8 @@
 #Create a program to calculate the area of a circle given its radius.
+
+radius=float(input("Enter the Radius of circle: "))
+pi = 3.14159
+
+area=pi*radius*radius
+
+print("The Area of the Circle is ",area)
